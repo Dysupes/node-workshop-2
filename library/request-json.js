32d5eb386cd848d1831001ef93c3275f32be94c6
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-12 19:02:22 +0000

    Added library folder with request-json

---
+++ library/request-json.js
@@ -0,0 +1,18 @@
+function requestJson(url,callback){
+    request(url, function(err, res){
+        if(err){
+            callback(err);
+        } else {
+            try {
+                var parseBody = JSON.parse(res.body);
+                callback(null,parseBody);
+            } catch(err) {
+                callback(err);
+            }        
+        }
+    });
+}
+
+module.exports = {
+    requestJson: requestJson
+};
