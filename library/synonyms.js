a226e7cd201263d7e044bb9a33a3615dbb9412ec
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-13 14:58:27 +0000

    Added text colors and emojis

--- library/synonyms.js
+++ library/synonyms.js
@@ -1,18 +1,20 @@
 var SynonymMaker = require('../get-synonyms.js');
 var prompt = require('prompt');
+var emoji = require('node-emoji');
+var colors = require('colors');
 var synonym = new SynonymMaker('e9848ae132571b1842b6c7c98e428530');
 prompt.start();
 prompt.get('userWord', function(err, content){
         if(err){
-        console.log('You have encountered an error.');
+        console.log((emoji.emojify('You have encountered an error. :grimacing:')));
         } else{
             console.log(content.userWord);
             synonym.get(content.userWord, function(err, result){
                 if(err){
                     console.log(err);
                 }
-                    console.log('Here are the synonyms of ' + content.userWord + ' : ' + result.adjective.syn);
-                    console.log('Here are the antonyms of ' + content.userWord + ' : ' + result.adjective.ant);
+                    console.log((emoji.emojify(':stuck_out_tongue_winking_eye:' + 'Here are the synonyms of '.rainbow + content.userWord.rainbow + ' : '.rainbow + result.adjective.syn + ':laughing:')));
+                    console.log((emoji.emojify(':grimacing:' + 'Here are the antonyms of '.rainbow + content.userWord.rainbow + ' : '.rainbow + result.adjective.ant + ":cold_sweat:")));
             });
             
         }
